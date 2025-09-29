94e44f7cd2c492657111ca2ac9bb9a541a197a77
Jan Mikeš <j.mikes@me.com>
2025-09-29 22:45:02 +0200

    Styling

--- frontend/assets/controllers/table_scroll_controller.js
+++ frontend/assets/controllers/table_scroll_controller.js
@@ -1,13 +1,19 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['table'];
+    static targets = ['scrollable'];
 
     connect() {
         this.checkScrollable();
         this.addEventListeners();
         this.throttledScroll = this.throttle(this.handleScroll.bind(this), 16);
         this.throttledResize = this.throttle(this.checkScrollable.bind(this), 100);
+
+        // Debug info
+        console.log('Table scroll controller connected', {
+            element: this.element,
+            scrollableTarget: this.scrollableTarget
+        });
     }
 
     disconnect() {
@@ -15,17 +21,30 @@ export default class extends Controller {
     }
 
     addEventListeners() {
-        this.element.addEventListener('scroll', this.throttledScroll);
+        if (this.hasScrollableTarget) {
+            this.scrollableTarget.addEventListener('scroll', this.throttledScroll);
+        }
         window.addEventListener('resize', this.throttledResize);
     }
 
     removeEventListeners() {
-        this.element.removeEventListener('scroll', this.throttledScroll);
+        if (this.hasScrollableTarget) {
+            this.scrollableTarget.removeEventListener('scroll', this.throttledScroll);
+        }
         window.removeEventListener('resize', this.throttledResize);
     }
 
     checkScrollable() {
-        const isScrollable = this.element.scrollWidth > this.element.clientWidth;
+        if (!this.hasScrollableTarget) return;
+
+        const scrollableElement = this.scrollableTarget;
+        const isScrollable = scrollableElement.scrollWidth > scrollableElement.clientWidth;
+
+        console.log('Checking scrollable:', {
+            scrollWidth: scrollableElement.scrollWidth,
+            clientWidth: scrollableElement.clientWidth,
+            isScrollable
+        });
 
         this.element.classList.toggle('is-scrollable', isScrollable);
 
@@ -43,14 +62,24 @@ export default class extends Controller {
     }
 
     updateScrollIndicators() {
-        const scrollLeft = this.element.scrollLeft;
-        const maxScrollLeft = this.element.scrollWidth - this.element.clientWidth;
+        if (!this.hasScrollableTarget) return;
+
+        const scrollableElement = this.scrollableTarget;
+        const scrollLeft = scrollableElement.scrollLeft;
+        const maxScrollLeft = scrollableElement.scrollWidth - scrollableElement.clientWidth;
 
         // Show left shadow when scrolled away from the start
         const showLeftShadow = scrollLeft > 5;
 
         // Show right shadow when there's more content to scroll to the right
         const showRightShadow = scrollLeft < maxScrollLeft - 5;
+
+        console.log('Updating indicators:', {
+            scrollLeft,
+            maxScrollLeft,
+            showLeftShadow,
+            showRightShadow
+        });
 
         this.element.classList.toggle('show-left-shadow', showLeftShadow);
         this.element.classList.toggle('show-right-shadow', showRightShadow);
